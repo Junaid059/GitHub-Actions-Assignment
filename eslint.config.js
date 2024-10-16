f27036e57b26117dc0b25e54f9f4f529ccbe57a4
junaid khalid <junaidkhalid2001@gmail.com>
2024-10-16 10:57:34 +0500

    added code review workflow

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,17 @@ import pluginJs from "@eslint/js";
 
 export default [
   {
-    languageOptions: { globals: globals.browser },
-    rules: {},
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Add Node.js globals
+        ...globals.jest, // Add Jest globals if you're testing
+      },
+      parserOptions: {
+        ecmaVersion: 2021, // Use modern ECMAScript features
+        sourceType: "module", // Enable ES modules
+      },
+    },
   },
   pluginJs.configs.recommended,
 ];
